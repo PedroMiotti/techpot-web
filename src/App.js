27e920340d2caf66d1cebc76f9755046178d65b6
Pedro Miotti <pedromiotti7@gmail.com>
2020-08-28 10:44:40 -0300

    Merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
-
-
+import React from "react";
+import "./App.css";
 
 // Pages
-  import FeedGrupo from './pages/FeedGrupo/index'
-  import Login from './pages/Login'
+import FeedGrupo from "./pages/FeedGrupo/index";
+import Login from "./pages/Login";
+
 
 const App = () => {
   return (
-
     <div className="App">
-        {/* <FeedGrupo /> */}
-        <Login />
-    </div>
+      <FeedGrupo />
+      {/* <Login /> */}
 
+
+    </div>
   );
-}
+};
 
 export default App;
