72f3cfe67118173fab4c3737860f4bf239f62e4d
Pedro Miotti <pedromiotti7@gmail.com>
2020-08-30 19:46:10 -0300

    Implemented react-router

---
+++ src/Router.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+
+// Router
+import { Switch, Route } from 'react-router-dom';
+
+// Componentes
+  import Navbar from './components/Navbar/index';
+  import BottomNavbar from './components/BottomNavbar/index';
+
+
+// Pages
+  import FeedGrupo from './pages/FeedGrupo/index';
+  import Login from './pages/Login';
+  import FeedPrincipal from './pages/FeedPrincipal';
+
+const Routes = () => {
+  return (
+      <Fragment>      
+        <Navbar />
+
+        <Switch>
+
+            <Route exact path="/" component={FeedPrincipal}/>
+            
+            <Route path="/grupo/feed" component={FeedGrupo}/>
+            
+            <Route path="/login" component={Login} />
+
+        </Switch>
+            
+        <BottomNavbar />
+            
+    </Fragment>
+
+    
+  );
+};
+
+export default Routes;
