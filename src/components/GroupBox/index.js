d9adf0805e1a7ee94b552c05e4f023d634159dbd
Feranda Shinoda <fernanda.shinoda@hotmail.com>
2020-08-31 14:47:58 -0300

    prop data added

--- src/components/GroupBox/index.js
+++ src/components/GroupBox/index.js
@@ -3,8 +3,14 @@ import './index.css';
 
 
 
-const GroupBox = () =>{
-    return(<button className="button"></button>)
+const GroupBox = ({data}) =>{
+    return(
+    <div id="container-groupBox">
+        {data ? <h1 id="groupBox-dataHeader"class="font-techpot">{data}</h1> : null}
+        <button className="button"></button>
+    </div>    
+        
+    )
 };
 
 export default GroupBox;
