226b015dc65a6af9da4c16a73186db34e77dc16f
Feranda Shinoda <fernanda.shinoda@hotmail.com>
2020-08-31 14:50:50 -0300

    renomeado prop box->tituloBoxList

--- src/components/GroupList/index.js
+++ src/components/GroupList/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './index.css';
 import GroupBox from '../GroupBox/index';
 
-const GroupList = ({titulo}) => {
+const GroupList = ({tituloBoxList}) => {
     return(
         <div class="containerGroup">
             {/*Titulo do container*/}
-            <h1 id="containerHeader">{titulo}</h1>
-
+            
+            <h1 id="containerHeader" class="font-techpot">{tituloBoxList}</h1>
+            
             {/*A linhazinha que separa o titulo das boxes*/}
             <div className="separator"></div>
             
             <div className="div-group-box">
                 {/*Box dos grupos em que o usuario pertence*/}
-                <GroupBox/>
-                <GroupBox/>
-                <GroupBox/>
-                <GroupBox/>
-                <GroupBox/>
-                <GroupBox/>
-                <GroupBox/>
+                <GroupBox data="31/08/2020"/>
+                <GroupBox data="07/09/2020"/>
+                <GroupBox data="11/09/2020"/>
+                <GroupBox data="15/09/2020"/>
+                <GroupBox data="31/09/2020"/>
+                <GroupBox data="08/10/2020"/>
+                <GroupBox data="21/10/2020"/>
             </div>
         </div>
     )
