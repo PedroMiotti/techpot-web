f190f695342c8214ed1ea03fb948d1d518fa5238
Feranda Shinoda <fernanda.shinoda@hotmail.com>
2020-08-31 22:00:38 -0300

    component perfil stantard

--- src/pages/perfilUsuario/index.js
+++ src/pages/perfilUsuario/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import InfoUsuario from '../../components/info-usuario/index.js';
 
-class perfilUsuario extends Component {
-    render() {
-        return(
-            <div>
-                <InfoUsuario />
-            </div>
-        );
-    }
+const PerfilUsuario = () => {
+    return(
+        <div>
+            <InfoUsuario />
+        </div>
+    );
+    
 }
 
-export default perfilUsuario;
+export default PerfilUsuario;
