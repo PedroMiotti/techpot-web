ea8ef82b0affd5d0d1b2497e1511e5ea56c9b5bb
Pedro <pedromiotti7@gmail.com>
2020-11-27 15:10:21 -0300

    Sidenav animation

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,47 +2,50 @@ import React from 'react';
 import './index.css'
 
 // Assets
-    import sidebarUserTestPicture from '../../assets/Rafa.jpg';
+import sidebarUserTestPicture from '../../assets/Rafa.jpg';
 
 // Components
-    import GroupListContainer from '../../shared/GroupListContainer/index'
+import GroupListContainer from '../../shared/GroupListContainer/index'
+
+// React Awesome
+import { Slide } from "react-awesome-reveal";
 
 // Icons
-    import { Close } from '@material-ui/icons'
-    
-
+import { Close } from '@material-ui/icons'
 
 const Sidebar = ({ onClose }) => {
-    return( 
-        <div className="sidebarContainer font-techpot">
-            <div className="sidebarUserInfoContainer">
-                <div className="sidebarCloseContainer" onClick={onClose}>
-                    <Close style={{color: '#fff', fontSize: '30px'}}/>
-                </div>
-                <div className="sidebarProfilePictureContainer">
-                        <img src={sidebarUserTestPicture} alt="sidebarUserTestPicture" className="sidebarUserPicture"/>
-                </div>
+    return (
+        <Slide >
+            <div className="sidebarContainer font-techpot">
+                <div className="sidebarUserInfoContainer">
+                    <div className="sidebarCloseContainer" onClick={onClose}>
+                        <Close style={{ color: '#fff', fontSize: '30px' }} />
+                    </div>
+                    <div className="sidebarProfilePictureContainer">
+                        <img src={sidebarUserTestPicture} alt="sidebarUserTestPicture" className="sidebarUserPicture" />
+                    </div>
 
-                <div className="sidebarUserDescContainer">
-                    <h3>Carlos Rafael</h3>
-                    <p>Professor</p>
-                </div>
+                    <div className="sidebarUserDescContainer">
+                        <h3>Carlos Rafael</h3>
+                        <p>Professor</p>
+                    </div>
 
+                </div>
+                <div className="sidebarGroupsContainer">
+                    <h2>Grupos</h2>
+
+                    <div className="sidebarGroupsList">
+                        <GroupListContainer />
+                        <GroupListContainer />
+                        <GroupListContainer />
+                        <GroupListContainer />
+                        <GroupListContainer />
+                        <GroupListContainer />
+
+                    </div>
+                </div>
             </div>
-            <div className="sidebarGroupsContainer">
-                <h2>Grupos</h2>
-
-                <div className="sidebarGroupsList">
-                    <GroupListContainer />
-                    <GroupListContainer />
-                    <GroupListContainer />
-                    <GroupListContainer />
-                    <GroupListContainer />
-                    <GroupListContainer />
-
-                </div>
-            </div>
-        </div>
+        </Slide>
     )
 }
 
